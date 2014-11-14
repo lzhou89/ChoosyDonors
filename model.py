8b29091ff96f282d4327a941ae52e79ae15ca8e0
Linda Zhou <lzhou@Lindas-MacBook-Pro.local>
2014-11-13 18:45:26 -0800

    updated model & seed file based on joel's feedback

--- model.py
+++ model.py
@@ -4,10 +4,10 @@ from sqlalchemy import Column, Integer, String, DateTime, Text, Float, Boolean, 
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, backref
-import correlation
 
 
-engine = create_engine("sqlite:///ratings.db", echo=False)
+
+engine = create_engine("sqlite:///ratings.db", echo=True)
 session = scoped_session(sessionmaker(bind=engine, 
                                       autocommit = False,
                                       autoflush = False))
@@ -28,30 +28,31 @@ class Project(Base):
     total_price = Column(Float, nullable=True)
     students_reached = Column(Integer, nullable=True)
     percent_funded = Column(Float, nullable=True)
-    num_donors = Column(Integer, nullable=True)
+    # num_donors = Column(Integer, nullable=True)
     matching = Column(Boolean, nullable=True)
-    funding_status = Column(String(20), nullable=True)
+    # funding_status = Column(String(20), nullable=True)
     fund_url = Column(String(120), nullable=True)
     proposal_url = Column(String(120), nullable=True)
     image_url = Column(String(120), nullable=True)
     thumb_image_url = Column(String(120), nullable=True)
     date_expiration = Column(DateTime, nullable=True)
 
-    teacher = relationship("teacher", backref=backref("projects", order_by=id))
-    school = relationship("school", backref=backref("projects", order_by=id))
+    teacher = relationship("Teacher", backref=backref("projects", order_by=id))
+    school = relationship("School", backref=backref("projects", order_by=id))
+
+    portfolioers = relationship("Donors", secondary="portfolios")   # [ Donor(), Donor() ]
 
 class ProjSub(Base):
     __tablename__ = "project_subjects"
 
     id = Column(Integer, primary_key = True)
     project_id = Column(String(64), ForeignKey('projects.id'), nullable=False)
-    subject_id = Column(Integer, nullable=True)
     subject = Column(String, nullable=False)
     focus_area = Column(String, nullable=True)
-    focus_area_id = Column(Integer, nullable=True)
     primary = Column(Boolean, nullable=True)
 
     project = relationship("project", backref=backref("project_subjects", order_by=id))
+
        
 class Teacher(Base):
     __tablename__ = "teachers"
@@ -59,7 +60,7 @@ class Teacher(Base):
     id = Column(String(64), primary_key = True)
     teacher_name = Column(String(30), nullable=True)
     school_id = Column(String(64), ForeignKey('schools.id'), nullable=False)
-    donations = Column(Integer, nullable=True)
+    # total_donations = Column(Integer, nullable=True)   # FIXME better name? or kill this if calculable from donations table?
     description = Column(Text, nullable=True)
     tfa = Column(Boolean, nullable=True)
     ny_teach = Column(Boolean, nullable=True)
@@ -76,12 +77,12 @@ class School(Base):
 
     id = Column(String(64), primary_key=True)
     school_name = Column(String(30), nullable=True)
-    nces_id = Column(BigInteger, nullable=True)
+    nces_id = Column(String(20), nullable=True)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     city = Column(String(40), nullable=True)
     state = Column(String(2), nullable=True)
-    zip = Column(String(15), nullable=True)
+    zipcode = Column(String(15), nullable=True)
     metro = Column(String(20), nullable=True)
     district = Column(String(64), nullable=True)
     county = Column(String(64), nullable=True)
@@ -95,14 +96,14 @@ class School(Base):
     grad_rate = Column(Float, nullable=True)
     nclb = Column(Boolean, nullable=True)
     student_teacher_ratio = Column(Integer, nullable=True)
-    free_lunch = Column(Integer, nullable=True)
-    test_results = Column(Integer, nullable=True)
+    percent_free_lunch = Column(Integer, nullable=True) 
+    edcom_test_rating = Column(Integer, nullable=True) 
     pov_level = Column(Float, nullable=True)
     crime_rate = Column(Float, nullable=True)
     impact_score = Column(Float, nullable=True)
     grade_levels = Column(String(64), nullable=True)
     school_url = Column(String(120), nullable=True)
-    total_proposals = Column(Integer, nullable=True)
+    # total_proposals = Column(Integer, nullable=True)  # prob calculable?
 
 class Supporter(Base):
     __tablename__ = "supporters"
@@ -114,6 +115,10 @@ class Supporter(Base):
     school = relationship("school", backref=backref("supporters", order_by=id))
     donor = relationship("donor", backref=backref("supporter_of", order_by=id))
 
+
+# calculate this
+# SELECT donor FROM joined_donation GROUP BY donor, school HAVING count(*) >= 3
+
 class Angel(Base):
     __tablename__ = "angels"
 
@@ -124,6 +129,8 @@ class Angel(Base):
     school = relationship("school", backref=backref("angels", order_by=id))
     donor = relationship("donor", backref=backref("angel_of", order_by=id))
 
+
+# move onto project?
 class Essay(Base):
     __tablename__ = "essays"
 
@@ -143,16 +150,18 @@ class Donor(Base):
     password = Column(String(64), nullable=True)
     city = Column(String(64), nullable=True)
     state = Column(String(2), nullable=True)
-    zip = Column(String(15), nullable=True)
+    zipcode = Column(String(15), nullable=True)
     is_teacher = Column(Boolean, nullable=True)
-    num_students = Column(Integer, nullable=True)
+    num_students = Column(Integer, nullable=True)  # calculable?
     name = Column(String(64), nullable=True)
-    total_proposals = Column(Integer, nullable=True)
+    # total_proposals = Column(Integer, nullable=True)  # calculable?
     donor_photo_url = Column(String(120), nullable=True)
-    total_supporters = Column(Integer, nullable=True)
+    # total_supporters = Column(Integer, nullable=True)  # FIXME JOEL
     num_donors_acquired = Column(Integer, nullable=True)
     donor_profile_url = Column(String(120), nullable=True)
-    num_proposals_supported = Column(Integer, nullable=True)
+    # num_proposals_supported = Column(Integer, nullable=True)
+
+    portfolioprojects = relationship("Project", secondary="portfolios")   # [ Project, Project ]
 
 class Donation(Base):
     __tablename__ = "donations"
@@ -160,11 +169,11 @@ class Donation(Base):
     id = Column(String(64), primary_key=True)
     project_id = Column(String(64), ForeignKey('projects.id'), nullable=False)
     donor_id = Column(String(64), ForeignKey('donors.id'), nullable=False)
-    donation_to_project = Column(Float, nullable=True)
-    donation_optional_support = Column(Float, nullable=True)
-    donation_total = Column(Float, nullable=True)
-    dollar_amount = Column(String(64), nullable=True)
-    donation_included_optional_support = Column(Boolean, nullable=True)
+    donation_to_project = Column(Float, nullable=True)  # gave to project?
+    donation_optional_support = Column(Float, nullable=True)  # 
+    # donation_total = Column(Float, nullable=True)   # calculate
+    # dollar_amount = Column(String(64), nullable=True)  # calculate
+    # donation_included_optional_support = Column(Boolean, nullable=True)
     used_acct_credit = Column(Boolean, nullable=True)
     used_campaign_gift_card = Column(Boolean, nullable=True)
     used_web_purchased_gift_card = Column(Boolean, nullable=True)
--- seed.py
+++ seed.py
@@ -2,6 +2,13 @@ import model
 import csv
 from datetime import datetime
 
+def tf_to_bool(val):
+    if val == "f":
+        return False
+    if val == "t":
+        return True
+    return None
+
 def load_projects(session):
 
     with open('opendata_projects.csv', 'rb') as csvfile:
@@ -16,10 +23,11 @@ def load_projects(session):
             p.total_price = float(line[32])
             p.students_reached = int(line[34])
             p.percent_funded = float(line[35])/float(line[32])
-            p.num_donors = int(line[36])
+            p.matching = tf_to_bool(line[37]) or tf_to_bool(line[38])
+            # p.num_donors = int(line[36])
             # if line[37] != "f" or line[38] != "f":
             #     p.matching = True
-            p.funding_status = line[39] #should I determine based on percent funded?
+            # p.funding_status = line[39] #should I determine based on percent funded?
             if line[43]:
                 p.date_expiration = datetime.strptime(line[43], "%Y-%m-%d")
             # session.add(p)
@@ -40,54 +48,34 @@ def load_projects(session):
             t = model.Teacher()
             t.id = line[1]
             t.school_id = line[2]
-            # if line[19] != "f":
-            #     t.tfa = True
-            # if line[20] != "f":
-            #     t.ny_teach = True 
             # session.add(t)
+            t.tfa = tf_to_bool(line[19])
+            t.ny_teach = tf_to_bool(line[20])
             s = model.School()
             s.id = line[2]
-            s.nces_id = int(line[3]) #should this actually be an int?
+            s.nces_id = line[3]
             s.latitude = float(line[4])
             s.longitude = float(line[5])
             s.city = line[6]
             s.state = line[7]
-            s.zip = line[8]
+            s.zipcode = line[8]
             s.metro = line[9]
             s.district = line[10]
             s.county = line[11]
-            boolean_fields = {12: "s.school_charter", 
-                              13: "s.school_magnet",
-                              14: "s.school_year_round",
-                              15: "s.school_nlns",
-                              16: "s.school_kipp",
-                              17: "s.school_charter_ready_promise",
-                              19: "t.tfa",
-                              20: "t.ny_teach",
-                              37: "p.matching",
-                              38: "p.matching"}
-            for key, value in boolean_fields.iteritems():
-                if line[key] == "t":
-                    value = True
-                elif line[key] == "f":
-                    value = False
-            # if line[12] != "f":
-            #     s.school_charter = true
-            # if line[13] != "f":
-            #     s.school_magnet = true
-            # if line[14] != "f":
-            #     s.school_year_round = true
-            # if line[15] != "f":
-            #     s.school_nlns = true
-            # if line[16] != "f":
-            #     s.school_kipp = true
-            # if line[17] != "f":
-            #     s.school_charter_ready_promise = true
+            # s.school_charter = (line[12] == "t")
+            s.school_charter = tf_to_bool(line[12])
+            s.school_magnet = tf_to_bool(line[13])
+            s.school_year_round = tf_to_bool(line[14])
+            s.school_nlns = tf_to_bool(line[15])
+            s.school_kipp = tf_to_bool(line[16])
+            s.school_charter_ready_promise = tf_to_bool(line[17])
             s.dc_pov = line[26]
             
-            session.add(p)
-            session.add(t)
-            session.add(s)
+            # session.add(p)
+            # session.add(t)
+            # session.add(s)
+            session.add_all([p,t,s])
+        session.commit()
     
 def load_essays(session):
     with open('opendata_essays.csv', 'rb') as csvfile:
@@ -100,6 +88,7 @@ def load_essays(session):
             e.synopsis = line[5]
             e.title = line[2]
             session.add(e)
+        session.commit()
 
 def load_donations(session):
     with open('opendata_donations.csv', 'rb') as csvfile:
@@ -109,39 +98,35 @@ def load_donations(session):
             d1.id = line[2]
             d1.city = line[4]
             d1.state = line[5]
-            d1.zip = line[6]
+            d1.zipcode = line[6]
+            d1.is_teacher = tf_to_bool(line[7])
             d2 = model.Donation()
             d2.id = line[0]
             d2.project_id = line[1]
             d2.donor_id = line[2]
             d2.donation_to_project = float(line[9])
             d2.donation_optional_support = float(line[10])
-            d2.donation_total = float(line[11])
-            d2.dollar_amount = line[12]
-            boolean_fields = {7: "d1.is_teacher",
-                              13: "d2.donation_included_optional_support",
-                              15: "d2.used_acct_credit",
-                              16: "d2.used_campaign_gift_card",
-                              17: "d2.used_web_purchased_gift_card",
-                              18: "d2.payment_was_promo_matched",
-                              19: "d2.via_giving_page",
-                              20: "d2.for_honoree"}
-            for key, value in boolean_fields.iteritems():
-                if line[key] == "t":
-                    value = True
-                elif line[key] == "f":
-                    value = False
+            # d2.donation_total = float(line[11])
+            # d2.dollar_amount = line[12]
+            d2.used_acct_credit = tf_to_bool(line[15])
+            d2.used_campaign_gift_card = tf_to_bool(line[16])
+            d2.used_web_purchased_gift_card = tf_to_bool(line[17])
+            d2.payment_was_promo_matched = tf_to_bool(line[18])
+            d2.via_giving_page = tf_to_bool(line[19])
+            d2.for_honoree = tf_to_bool(line[20])
             session.add(d1)
             session.add(d2)
+        session.commit()
             
 def load_poverty_levels(session):
     with open('pov_levels.csv', 'rb') as csvfile:
-        all_pov_levels = sv.reader(csvfile, delimiter = ",")
+        all_pov_levels = csv.reader(csvfile, delimiter = ",")
         for line in all_pov_levels:
-            query = session.query(model.School).filter_by(zip = line[0]).all()
+            query = session.query(model.School).filter_by(zipcode=line[0]).all()
+            poverty_level = float(line[1])
             for school in query:
-                school.pov_level = float(line[1])
-                #session action?
+                school.pov_level = poverty_level
+        session.commit()
 
 
 
@@ -163,7 +148,6 @@ def main(session):
     # You'll call each of the load_* functions with the session as an argument
     load_projects(session)
 
-    session.commit()
 
 
 
