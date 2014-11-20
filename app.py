55e54d3ac6bd7bb832f8e7c385b14b1c42a682ff
Hackbright Student <info@hackbrightacademy.com>
2014-11-20 15:59:46 -0800

    Added html pages

--- app.py
+++ app.py
@@ -13,6 +13,18 @@ def index():
 def show_portfolio_choices():
 	return render_template("clusters.html")
 
+@app.route("/confirm_portfolio")
+def confirm_portfolio():
+    return render_template("portfolio_confirmation.html")
+
+@app.route("/project_search")
+def search_single_project():
+    return render_template("single_project.html")
+
+@app.route("/project")
+def show_project():
+    return render_template("project_page.html")
+
 @app.route("/login", methods=["GET"])
 def show_login():
     if "user_id" in session:
@@ -33,6 +45,23 @@ def login():
     else:
          flash("Username not found")
          return redirect("/login") 
+
+@app.route("/profile")
+def show_profile():
+    return render_template("donor_profile.html")
+
+@app.route("/impact")
+def show_impact():
+    return render_template("impact.html")
+
+#To Do:
+#add donate button & check how to submit donation
+#find picture
+#cluster visualization
+#js to enter number & show certain # of results
+#ability to check to keep a project for portfolio
+#confirm page remove individual projects
+
 
 if __name__ == "__main__":
 	app.run(debug = True)
