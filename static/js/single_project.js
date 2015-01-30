0dc8e1e703253b1bbb524a9a7fc0ce4462e7bb3d
Linda Zhou <lzhou@lindas-mbp.home>
2015-01-30 07:17:52 -0500

    streamlined code

--- static/js/single_project.js
+++ static/js/single_project.js
@@ -26,61 +26,7 @@ $(document).ready(function(){
 /*search*/
 function getSearchTerms(e){
   e.preventDefault();
-  var input = {};
-  var zipArea = $("#zipcode");
-  if(zipArea.val()!==""){
-    input["zipcode"] = zipArea.val();
-  }
-
-  var selected = [];
-  $("input:checked").each(function() {
-    selected.push($(this).attr('id'));
-  });
-  if(selected.length > 0){
-    selected = selected.toString();
-    input["checkboxes"] = selected;
-  }
-  console.log(selected);
-  console.log(input);
-
-  $.ajax({
-    type: "POST",
-    url: "/keyword_search",
-    data: input
-  })
-  .done(function(results) {
-    var output = JSON.parse(results);
-    $('#results').html("<ul class='list' id='list_results'></ul>");
-
-    for (var i=0; i < output.length; i++) {
-      var rDiv = document.createElement('div');
-      rDiv.className = "well well-sm";
-      $('#list_results').append(rDiv);
-      if (output.length === 0) {
-      rDiv.innerHTML = "No results found.";
-      break;
-      } else{
-        rDiv.innerHTML = "<dd><a href='/project/"+output[i]["id"]+
-            "'>"+output[i]["title"]+"</a></dd>"+
-            "<dt class='inline'>School Location:</dt><dd>"+output[i]["location"]+"</dd>"+
-            "<dt class='inline'>Grade Level:</dt><dd>"+output[i]["grade"]+"</dd>"+
-            "<dt>Needs:</dt><dd>"+output[i]["needs"]+"</dd>"+
-          "</dl>";
-      }
- 
-    }
-    var rUl = document.createElement('ul');
-      rUl.className = "pagination";
-      $('#results').append(rUl);
-      var options = {
-        valueNames: ['well well-sm'],
-        page: 5,
-        plugins: [
-          ListPagination({})
-        ]
-      };
-      var listObj = new List('results', options);
-  });
+  getCheckboxes();
 }
 
 function getCheckboxes(){
